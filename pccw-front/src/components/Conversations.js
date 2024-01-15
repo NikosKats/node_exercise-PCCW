bad712d6f560d25bfb674fdd2785c9a0dbefcc9d
NikosKats <26749680+NikosKats@users.noreply.github.com>
2024-01-15 21:37:47 +0200

    edits

--- pccw-front/src/components/Conversations.js
+++ pccw-front/src/components/Conversations.js
@@ -71,24 +71,23 @@ const Conversations = ({ fetchConversations, fetchMessagesExchange, selectConver
 
   console.log("messagesExchange = ", messagesExchange)
 
-  const sortedUsers = users;
+  const conversations = [
+    { id: 1, name: 'Alice', latestMessage: 'Hi there!', avatar: 'path/to/avatar1.jpg' },
+    { id: 2, name: 'Bob', latestMessage: 'How are you?', avatar: 'path/to/avatar2.jpg' },
+
+  ];
 
   return (
     <div className="conversations-list">
-      {sortedUsers.map(user => (
-        <div key={user.id} className="user" onClick={() => onUserClick(user.id)}>
-          <img src={`path/to/avatar/for/user/${user.id}`} alt={user.name} className="avatar" />
-          <div className="user-info">
-            <h5>{`${user.name} ${user.surname}`}</h5>
-            <p>{`DOB: ${user.dateOfBirth}`}</p>
-            <p>{`Gender: ${user.gender}`}</p>
-            <p>{`Username: ${user.username}`}</p>
-            <p>{`Created At: ${user.createdAt}`}</p>
-            <p>{`Updated At: ${user.updatedAt}`}</p>
+      {conversations.map(conversation => (
+        <div key={conversation.id} className="conversation" onClick={() => onConversationClick(conversation.id)}>
+          <img src={conversation.avatar} alt={conversation.name} className="avatar" />
+          <div className="conversation-info">
+            <h5>{conversation.name}</h5>
+            <p>{conversation.latestMessage}</p>
           </div>
         </div>
       ))}
-
     </div>
   );
 }
