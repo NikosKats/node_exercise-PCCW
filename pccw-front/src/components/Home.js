65fdd90db7a94e8745f9049f8a2732e9751024e0
NikosKats <26749680+NikosKats@users.noreply.github.com>
2024-01-15 22:30:52 +0200

    edits

--- pccw-front/src/components/Home.js
+++ pccw-front/src/components/Home.js
@@ -7,16 +7,16 @@ import Conversations from './Conversations';
 
 function Home() {
 
-  const handleConversationClick = (conversationId) => {
-    console.log("Conversation clicked:", conversationId);
+ const handleConversationClick = (conversationId) => {
+  console.log("Conversation clicked:", conversationId);
+
+  // Construct the URL
+  const url = `http://localhost:3000/?currentUserId=1&userID1=1&userID2=${conversationId}`;
   
-    // Construct the URL
-    const url = `http://localhost:3000/?currentUserId=1&userID1=1&userID2=${conversationId}`;
-    
-    // Redirect to the URL
-    window.location.href = url;
-  };
-  
+  // Redirect to the URL
+  window.location.href = url;
+};
+
 
   return (
     <div className="home-container">
