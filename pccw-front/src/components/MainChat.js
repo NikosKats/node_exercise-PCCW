b9cdc4702c4be073d9953a39b9a266ac0710862b
NikosKats <26749680+NikosKats@users.noreply.github.com>
2024-01-15 18:26:51 +0200

    redux rx

--- pccw-front/src/components/MainChat.js
+++ pccw-front/src/components/MainChat.js
@@ -12,9 +12,10 @@ const MainChat = ({ fetchMessagesExchange, selectMessagesExchange }) => {
   const queryParams = new URLSearchParams(location.search);
 
   // Extracting parameters from the URL
-  const currentUserId = queryParams.get('currentUserId') || '1';
-  const userID1 = queryParams.get('userID1') || '1';
-  const userID2 = queryParams.get('userID2') || '3';
+  const currentUserId = parseInt(queryParams.get('currentUserId')) || 1; 
+  const userID1 = parseInt(queryParams.get('userID1')) || 1;
+  const userID2 = parseInt(queryParams.get('userID2')) || 3;
+
 
   const payload = {
     userID1,
