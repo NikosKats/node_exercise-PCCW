f6278b792eb5f056a7c63ee5b5740d4c5087b817
NikosKats <26749680+NikosKats@users.noreply.github.com>
2024-01-15 21:19:50 +0200

    edits

--- pccw-front/src/models/selectors.js
+++ pccw-front/src/models/selectors.js
@@ -2,6 +2,6 @@
 
 export const selectMessagesExchange = (state) => state.messagesExchange;
 
-export const selectConversations = (state) => state.selectConversations;
+export const selectConversations = (state) => state.users;
 
 // () => ({}) // instead of return use parenthesis
