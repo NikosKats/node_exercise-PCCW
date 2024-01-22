ffa7d13adc639b96c0e713b3dc7e8eede683566f
NikosKats <26749680+NikosKats@users.noreply.github.com>
2024-01-22 19:07:36 +0200

    this commit solves the dependencies issue

--- pccw-front/src/store.js
+++ pccw-front/src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'; // inject rxjs middleware to store in order to watch the action stream
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import { createEpicMiddleware } from 'redux-observable';
 
 const epicMiddleware = createEpicMiddleware();
-const enhancers = composeWithDevTools(applyMiddleware(epicMiddleware));
+// const enhancers = composeWithDevTools(applyMiddleware(epicMiddleware));
+
+const enhancers = applyMiddleware(epicMiddleware);
 
 export const configureStore = (rootReducer, rootEpic) => {
   const store = createStore(rootReducer, {}, enhancers);
